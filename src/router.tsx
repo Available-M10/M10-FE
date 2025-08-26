255dfe16873684dd4610df8b3c80c2e28b0ce418
seeum0507 <kwonstella0507@gmail.com>
2025-08-27 00:47:20 +0900

    chore: Setting

---
+++ src/router.tsx
@@ -0,0 +1,8 @@
+import { createBrowserRouter } from "react-router-dom";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <></>, //페이지 개발하면 나중에 추가
+  },
+]);
---
+++ tailwind.config.js
@@ -0,0 +1,45 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html, js, tsx, jsx, ts}",
+    "./src/components/ui/**/*.{ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        // MAIN
+        main: {
+          0: "#FF8171",
+          100: "#FFE7E7",
+          200: "#FF7D68",
+          300: "#FF6D5A",
+        },
+
+        // GRAYSCALE
+        gray: {
+          0: "#F2F2F2",
+          100: "#757575",
+          200: "#585858",
+          300: "#9D9D9D",
+          400: "rgba(0,0,0,0.62)",
+          500: "rgba(0,0,0,0.4)",
+          600: "rgba(0,0,0,0.5)",
+          700: "#E6E6E6",
+        },
+
+        // ETC
+        black: "#000000",
+        red: "#FF1F1F",
+        green: "#FF4646",
+        brown: "rgba(138,112,73,0.8)",
+        white: "#FFFFFF",
+      },
+
+      // 폰트 설정
+      fontFamily: {
+        sans: ["Pretendard", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
