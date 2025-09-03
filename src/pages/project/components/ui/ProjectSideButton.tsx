1fe18a7efdb2b2bcf909488b2daab63335d2911a
tccmw <jungmwoo6248@gmail.com>
2025-09-03 21:26:34 +0900

    feat: project 전용 버튼 생성

---
+++ src/pages/project/components/ui/ProjectSideButton.tsx
@@ -0,0 +1,7 @@
+export function ButtonFlowSide() {
+  return (
+    <div className="w-[80%] md:w-[25%] h- border border-gray-250 rounded-md">
+      <button className="w-full h-full">ads</button>
+    </div>
+  );
+}
