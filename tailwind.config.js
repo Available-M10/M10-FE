5a688cee6f34137ac027fcd803e71ecf1b2b706b
seeum0507 <kwonstella0507@gmail.com>
2025-09-03 22:38:06 +0900

    chore: tailwind 에러 수정

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{html, js, tsx, jsx, ts}",
-    "./src/components/ui/**/*.{ts,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors: {
