2a68e7dde0fe588b7868744573907b39f2d93d41
tccmw <jungmwoo6248@gmail.com>
2025-09-04 18:16:19 +0900

    feat: project 헤더 생성

--- src/pages/project/components/layout/ProjectHead.tsx
+++ src/pages/project/components/layout/ProjectHead.tsx
@@ -1,3 +1,16 @@
+import { Button } from "../ui/button";
+import { LogoTitle } from "../ui/LogoTitle";
+
 export function ProjectHead() {
-  return <div className="bg-red h-10 border-0 border-button-2 border-gray-250"></div>
+  return (
+    <div className="h-[10%] border-b border-gray-250 flex justify-between px-5 text-xs">
+      <LogoTitle />
+      <div className="w-1/4 h-full flex items-center justify-center gap-x-3">
+        <Button label="저장" />
+        <Button label="테스트" />
+        <Button label="실행" bgColor="bg-[#FF6D5A]" />
+        <Button label="setting" />
+      </div>
+    </div>
+  );
 }
