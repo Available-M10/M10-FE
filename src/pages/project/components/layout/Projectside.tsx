be37547866456e6a2e4567d5e2671c9f3230a25b
tccmw <jungmwoo6248@gmail.com>
2025-09-04 16:42:15 +0900

    style: h 변경

--- src/pages/project/components/layout/Projectside.tsx
+++ src/pages/project/components/layout/Projectside.tsx
@@ -2,7 +2,7 @@ import { NodeSide } from "../NodeSide";
 
 export function ProjectSide() {
   return (
-    <div className="w-1/3 h-[calc(100%-2.5rem)] bg-white border border-gray-250 absolute top-10 right-0 z-10">
+    <div className="w-1/4 h-[calc(100%-10%)] bg-white border border-gray-250 absolute  right-0 z-10">
       <div className="w-full h-2/3 p-5 flex flex-col">
         <div>노드 속성</div>
         <div className="w-full h-[80%] flex flex-col justify-between pt-1">
