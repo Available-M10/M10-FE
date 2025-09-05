d9cb1cd4e25d1015b6578fe3f929e2e4bed0ec93
tccmw <jungmwoo6248@gmail.com>
2025-09-05 19:30:55 +0900

    chore: tabConfig 상수 추가

---
+++ src/pages/project/constants/tabConfig.ts
@@ -0,0 +1,27 @@
+// src/constants/tabConfig.ts
+import iconCilck from "@/assets/icon-click.svg";
+import iconAiagent from "@/assets/icon-Aiagent.svg";
+import iconEnd from "@/assets/icon-end.svg";
+import iconNote from "@/assets/icon-note-side.svg";
+import iconChat from "@/assets/icon-chat-side.svg";
+
+export const tabConfig: Record<
+  string,
+  {
+    first: { icon: string; text: string };
+    second: { icon: string; text: string };
+  }
+> = {
+  시작: {
+    first: { icon: iconCilck, text: "클릭 시 시작" },
+    second: { icon: iconChat, text: "채팅 시 시작" },
+  },
+  본론: {
+    first: { icon: iconAiagent, text: "Ai Agent" },
+    second: { icon: iconNote, text: "노트 작업" },
+  },
+  종료: {
+    first: { icon: iconEnd, text: "프로젝트 종료" },
+    second: { icon: "", text: "" },
+  },
+};
