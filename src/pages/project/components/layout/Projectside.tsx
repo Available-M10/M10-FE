46da150b08dd69ab08ddaeada60d365836f57053
tccmw <jungmwoo6248@gmail.com>
2025-09-05 19:30:33 +0900

    refactor: projectSide 구조 단순화

--- src/pages/project/components/layout/Projectside.tsx
+++ src/pages/project/components/layout/Projectside.tsx
@@ -1,14 +1,16 @@
 import { NodeSide } from "../NodeSide";
+import { ChatSide } from "../ChatSide";
+import { useProject } from "../../context/ProjectContext";
 
 export function ProjectSide() {
-  return (
-    <div className="w-1/4 h-[calc(100%-10%)] bg-white border border-gray-250 absolute  right-0 z-10">
-      <div className="w-full h-2/3 p-5 flex flex-col">
-        <div>노드 속성</div>
-        <div className="w-full h-[80%] flex flex-col justify-between pt-1">
-          <NodeSide />
-        </div>
-      </div>
-    </div>
-  );
+  const { side } = useProject();
+
+  switch (side) {
+    case "node":
+      return <NodeSide />;
+    case "chat":
+      return <ChatSide />;
+    default:
+      return null;
+  }
 }
