bc1cc8f8caadd14ce77e7e464906bd23a610c3a6
tccmw <jungmwoo6248@gmail.com>
2025-09-05 19:24:11 +0900

    feat: ProjectProvider 추가

--- src/pages/project/ProjectPage.tsx
+++ src/pages/project/ProjectPage.tsx
@@ -1,15 +1,18 @@
 import { ProjectHead } from "./components/layout/ProjectHead";
-import { ProjectFlow } from "./components/flows/ProjectFlow";
 import { ProjectSide } from "./components/layout/Projectside";
+import { ProjectFlow } from "./components/flows/ProjectFlow";
+import { ProjectProvider } from "./context/ProjectContext";
 
 export function ProjectPage() {
   return (
-    <div className="flex flex-col h-screen">
-      <ProjectHead />
-      <div className="flex flex-1">
-        <ProjectFlow />
-        <ProjectSide />
+    <ProjectProvider>
+      <div className="flex flex-col h-screen">
+        <ProjectHead />
+        <div className="flex flex-1">
+          <ProjectFlow />
+          <ProjectSide />
+        </div>
       </div>
-    </div>
+    </ProjectProvider>
   );
 }
