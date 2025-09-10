62f35bf2ce8fd8ccec45a8ef0af8cfd9ed2b2da7
tccmw <jungmwoo6248@gmail.com>
2025-09-10 19:23:14 +0900

    feat: header에 프로젝트 이름 추가

--- src/pages/project/components/ui/LogoTitle.tsx
+++ src/pages/project/components/ui/LogoTitle.tsx
@@ -5,6 +5,7 @@ export function LogoTitle() {
     <div className="h-full flex items-center gap-x-3">
       <img src={Logo} className="w-[10%]" />
       <div className="font-black text-xl text-[#FF6D5A]">M10</div>
+      <div>프로젝트 이름</div>
     </div>
   );
 }
