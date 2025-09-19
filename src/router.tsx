b8c6b9fba79581aef33ac35fdcf2e1e8ac13d963
seeum0507 <kwonstella0507@gmail.com>
2025-09-20 00:25:06 +0900

    refactor: 메인 폴더 구조 수정

--- src/router.tsx
+++ src/router.tsx
@@ -1,5 +1,5 @@
 import { createBrowserRouter } from "react-router-dom";
-import { MainPage } from "@/pages/MainPage";
+import { MainPage } from "@/pages/Main/MainPage";
 
 export const router = createBrowserRouter([
   {
