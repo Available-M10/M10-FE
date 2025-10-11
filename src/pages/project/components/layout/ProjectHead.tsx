1c24f157a65a6d54dfa0708a499ee7ed701b3a8d
tccmw <jungmwoo6248@gmail.com>
2025-10-11 22:33:12 +0900

    feat: 프로젝트Id

--- src/pages/project/components/layout/ProjectHead.tsx
+++ src/pages/project/components/layout/ProjectHead.tsx
@@ -2,23 +2,36 @@ import { IconButton } from "../ui/iconButton";
 import { TextButton } from "../ui/textButton";
 import { LogoTitle } from "../ui/LogoTitle";
 import { useProject } from "../../context/ProjectContext";
+import { NodeHandlers } from "../hooks/useNodeHandlers";
+import { useLLM } from "../../context/LLMContext";
+import { useNode } from "../flows/hooks/useNode";
+import type { projectIdProps } from "../../types/projectId";
 
-export function ProjectHead() {
+export function ProjectHead({ projectId }: projectIdProps) {
   const { setSide } = useProject();
+  const { prompt } = useLLM();
+  const { getNodePort, setNodePort } = useNode();
+
+  const { handleNoteClick } = NodeHandlers({
+    getNodePort,
+    setNodePort,
+    prompt,
+    // projectId
+  });
   return (
     <div className="h-[10%] border-b border-gray-250 flex justify-between px-5 text-xs">
       <LogoTitle />
-      <div className="w-1/4 h-full flex items-center justify-center gap-x-3">
+      <div className="w-1/6 h-full flex items-center justify-center gap-x-3">
         <IconButton iconName="icon-chat" onclick={() => setSide("chat")} />
         <IconButton
           iconName="icon-note-project"
           onclick={() => setSide("node")}
         />
-        <TextButton label="저장" onClick={() => setSide("save")} />
         <TextButton
           label="실행"
           bgColor="bg-[#FF6D5A]"
-          fontColor="text-white" onClick={() => setSide("start")}
+          fontColor="text-white"
+          onClick={handleNoteClick}
         />
       </div>
     </div>
